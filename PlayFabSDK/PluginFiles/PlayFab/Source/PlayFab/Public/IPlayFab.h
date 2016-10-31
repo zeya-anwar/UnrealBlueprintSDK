b1c8ea46f5a850bb0cd8377b72ad80911cd732d3
pgilmorepf <paul@playfab.com>
2016-10-31 10:32:16 -0700

    Version number for https://api.playfab.com/releaseNotes/#161031

--- PlayFabSDK/PluginFiles/PlayFab/Source/PlayFab/Public/IPlayFab.h
+++ PlayFabSDK/PluginFiles/PlayFab/Source/PlayFab/Public/IPlayFab.h
@@ -16,9 +16,9 @@ public:
     * @return Returns singleton instance, loading the module on demand if needed
     */
 
-    const FString SdkVersion = "0.0.161017";
+    const FString SdkVersion = "0.0.161031";
     const FString BuildIdentifier = "jbuild_unrealblueprintsdk_0";
-    const FString VersionString = "UnrealBlueprintSDK-0.0.161017";
+    const FString VersionString = "UnrealBlueprintSDK-0.0.161031";
 
     // Photon Ap Ids
     FString PhotonRealtimeAppId;
